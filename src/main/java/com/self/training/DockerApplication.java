542f4bf6244d6d6cc7fbcff2d3a77fe7c806c3a7
Bing ZHANG <dwendwen@Bings-MacBook-Pro-16.local>
2022-03-01 16:33:21 -0500

    add maven project

---
+++ src/main/java/com/self/training/DockerApplication.java
@@ -0,0 +1,15 @@
+package com.self.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dwendwen
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
